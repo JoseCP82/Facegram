cfd495f78e535416875ae297f2b2d0c21b7bdbfc
Juanmagw <juanma4x21@gmail.com>
2022-11-17 20:10:29 +0100

    Pruebas de Usuarios

--- src/main/java/com/facegram/controllers/LoginController.java
+++ src/main/java/com/facegram/controllers/LoginController.java
@@ -1,7 +1,12 @@
 package com.facegram.controllers;
 
 import com.facegram.App;
+import com.facegram.logging.Logging;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -11,40 +16,35 @@ import java.io.IOException;
 
 public class LoginController {
 
+    /**
+     * Atributos de LoginController
+     */
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    /**
+     * Elementos FXML
+     */
     private Button btnLogIn;
     private Button btnRegister;
-    private Button btnFeed;
     private TextField tfUser;
     private TextField tfPassword;
 
-    public void changeRegister(Stage stage) {
-        btnRegister.setOnMouseClicked(e->{
-            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("register.fxml"));
-            Scene scene = null;
-            try {
-                scene = new Scene(fxmlLoader.load(), 320, 240);
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-            stage.setTitle("Hello!");
-            stage.setScene(scene);
-            stage.show();
-        });
+    /*public void changeRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../../../../resources/com/facegram/register.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void changeFeed(Stage stage){
-        btnFeed.setOnMouseClicked(e->{
-            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("feed.fxml"));
-            Scene scene = null;
-            try {
-                scene = new Scene(fxmlLoader.load(), 320, 240);
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-            stage.setTitle("Hello!");
-            stage.setScene(scene);
-            stage.show();
-        });
-    }
+    public void changeFeed(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../../../../resources/com/facegram/feed.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }*/
 
 }
