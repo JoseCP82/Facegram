4783aadd29aeab4d4f23ce5fa9b75bc8ddc95658
JoseCP82 <josecamarap@gmail.com>
2022-11-23 23:20:36 +0100

    Implementando update post

--- src/main/java/com/facegram/controllers/App.java
+++ src/main/java/com/facegram/controllers/App.java
@@ -1,7 +1,5 @@
 package com.facegram.controllers;
 
-import com.facegram.model.DAO.UserDAO;
-import com.facegram.model.dataobject.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
